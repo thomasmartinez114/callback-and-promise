82fa2f03eb83ba85293e8d30756eff89d6c3214f
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-05-14 12:43:31 -0400

    pushed update

--- callback.js
+++ callback.js
@@ -1,6 +1,6 @@
 var result = undefined;
 
-function add(a, b, sum){
+function add(a, b, sum) {
     sum = a + b;
 }
 
@@ -10,7 +10,7 @@ setTimeout(() => add(2, 3, result), 1000);
 console.log(`Synchronous result: ${result}`);
 console.log(`Running some code right here`);
 
-function addWithCallBack(a, b, callback){
+function addWithCallBack(a, b, callback) {
     const sum = a + b;
     callback(sum);
 }
@@ -28,30 +28,27 @@ console.log("Running some more code");
 
 // Promises
 
-function addWithPromise (c, d,){
-const total = c + d;
-return total
-
+function addWithPromise(c, d, ) {
+    const total = c + d;
+    return total
 }
 
 
 let promise = new Promise(function(resolve, reject) {
     setTimeout(() => resolve(addWithPromise(20, 30)), 10);
-    
-  });
-  // resolve settimeout addwithPromises 5 +7 // 13 wait none
-  // settimeout resolve addwithpromsies 5 + 7 //13 
-  // resolve runs the first function in .then
-  promise.then(
-    result => console.log(`Promise successful here is the total ${result}`), 
-    error => console.log(error) 
-  );
 
-  let promise1 = new Promise(function(resolve, reject){
+});
+
+promise.then(
+    result => console.log(`Promise successful here is the total ${result}`),
+    error => console.log(error)
+);
+
+let promise1 = new Promise(function(resolve, reject) {
     setTimeout(() => resolve(addWithPromise(30, 30)), 0);
-  })
+})
 
-  promise1.then(
-      result => console.log(`Promise successful here is the total ${result}`), 
-      error => console.log(error)
-  )
+promise1.then(
+    result => console.log(`Promise successful here is the total ${result}`),
+    error => console.log(error)
+)
